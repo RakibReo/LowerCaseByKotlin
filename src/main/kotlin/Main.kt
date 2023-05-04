e4fdaa9e8ecaea4e210d65031cec039f09824fb8
SuRakib <rakibul15-10976@diu.edu.bd>
2023-05-04 19:48:56 +0600

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val chars: List<String> = listOf("SALONI","MESSI","ENZO","PARDES")
+
+    val lowercase = chars.map { it.lowercase()
+
+    }
+    println("The result is: $lowercase") // [A, Ω, 1, ʼN, A, +, SS]
+
+}
